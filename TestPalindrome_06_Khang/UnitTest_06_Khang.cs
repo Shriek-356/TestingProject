c72b879b5c07c817b6d67c54b15c23588ea99db1
Shriek-356 <khangvskiss@gmail.com>
2025-03-29 21:01:10 +0700

    update test case

--- TestPalindrome_06_Khang/UnitTest_06_Khang.cs
+++ TestPalindrome_06_Khang/UnitTest_06_Khang.cs
@@ -17,36 +17,42 @@ namespace TestPalindrome_06_Khang
         }
 
 
-        [TestMethod]
+        [TestMethod]//Test case 1, chuoi: madam la 1 chuoi palindrome => tra ra True => Passed
         public void ValidPalindrome_06_Khang_1()
         {
             Assert.IsTrue(checker_06_Khang.IsPalindrome_06_Khang("madam"));
             
         }
 
-        [TestMethod]
+        [TestMethod]//Test case 2, chuoi: abc khong la 1 chuoi palindrome => tra ra False, nhung ket qua mong doi lai tra ra True => Failed
         public void ValidPalindrome_06_Khang_2()
         {
-            Assert.IsTrue(checker_06_Khang.IsPalindrome_06_Khang("asd"));
+            Assert.IsTrue(checker_06_Khang.IsPalindrome_06_Khang("abc"));
 
         }
 
-        [TestMethod]
-        public void PalindromeWithSpaces_06_Khang()
+        [TestMethod]//Test case 3, chuoi: ma d   a m la 1 chuoi palindrome mac du co khoang trang => tra ra true => Passed
+        public void PalindromeWithSpaces_06_Khang_1()
         {
             Assert.IsTrue(checker_06_Khang.IsPalindrome_06_Khang("ma d   a m "));
         }
 
-        [TestMethod]
-        public void PalindromeWithoutWord_06_Khang()
+        [TestMethod]//Test case 4, chuoi: " aa   bb 2 d " khong la 1 chuoi palindrome mac du co khoang trang => tra ra False, nhung ket qua mong doi la True => Failed
+        public void PalindromeWithoutWord_06_Khang_2()
         {
-            Assert.IsFalse(checker_06_Khang.IsPalindrome_06_Khang("   "));
+            Assert.IsTrue(checker_06_Khang.IsPalindrome_06_Khang(" aa   bb 2 d "));
         }
 
-        [TestMethod]
-        public void PalindromeWithNull_06_Khang()
+        [TestMethod]//Test case 5, chuoi la null => tra ra False => Passed
+        public void PalindromeWithNull_06_Khang_1()
         {
-            Assert.IsTrue(checker_06_Khang.IsPalindrome_06_Khang(null));
+            Assert.IsFalse(checker_06_Khang.IsPalindrome_06_Khang(null));
+        }
+
+        [TestMethod]//Test case 6, chuoi la "aba" khac null => tra ra True => Failed 
+        public void PalindromeWithNull_06_Khang_2()
+        {
+            Assert.IsFalse(checker_06_Khang.IsPalindrome_06_Khang("aba"));
         }
     }
 }
